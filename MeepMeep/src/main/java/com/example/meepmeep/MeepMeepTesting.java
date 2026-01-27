1d538e045180c82b4edcededf62c888ebed3260e
P135928 <zackaryzpi@gmail.com>
2026-01-26 20:32:08 -0700

    Added MeepMeep and made an auto using roadunner, again can't test until wednesday but at least i've got meepmeep!

---
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepTesting.java
@@ -0,0 +1,26 @@
+package com.example.meepmeep;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity rustina = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        rustina.runAction(rustina.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                .lineToX(-10)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(rustina)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/code/roadrunnerAuto.java
@@ -0,0 +1,30 @@
+// This imports all the packages and libs.
+package org.firstinspires.ftc.teamcode.code;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import org.firstinspires.ftc.teamcode.MecanumDrive;
+
+@Autonomous
+public class roadrunnerAuto extends LinearOpMode {
+    @Override
+    public void runOpMode() {
+        // Initial pose
+        Pose2d beginPose = new Pose2d(0, 0, 0);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, beginPose);
+
+        // Build the trajectory to move back 10 inches using coordinates
+        Action robotAuto = drive.actionBuilder(beginPose)
+                .lineToX(-10)
+                .build();
+        // Wait for the driver to press start
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        // Execute the trajectory
+        Actions.runBlocking(robotAuto);
+    }
+}
